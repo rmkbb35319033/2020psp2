04014bc39cbb63f4b3b8f42e290214e5b62e10df
bb35319033 <63335486+bb35319033@users.noreply.github.com>
2020-04-20 21:27:30 +0900

    k01の完成

--- k01/k01.c
+++ k01/k01.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
-
+extern double ave_online(double val,double ave,int N);
+extern double var_online(double val, double ave, double square_ave,int N);
+int gen;
+int i=0;
+double AVE=0,AVE_BF=0,AVE_2=0,AVE_2BF,VAL=0,UVAL=0;
 int main(void)
 {
     double val;
@@ -23,15 +25,15 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
+    fgets(buf,sizeof(buf),fp);
     while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+        i++;
+        sscanf(buf,"%d, %lf",&gen,&val);
+        AVE_BF=AVE;//VALを求める際に、(N-1)番目の平均を使うためこの位置で(N-1)番目の平均を保存しておく。
+        AVE=ave_online(val,AVE,i);//平均を求める
+        AVE_2BF=AVE_2;//VALを求める際に、(N-1)番目の２乗の平均を使うためこの位置で(N-1)番目の２乗の平均を保存しておく。
+        AVE_2=ave_online(val*val,AVE_2,i);//２乗の平均を求める
+        VAL=var_online(val,AVE_BF,AVE_2BF,i);//分散を求める
     }
 
     if(fclose(fp) == EOF){
@@ -39,9 +41,24 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    printf("AVE=%lf\n",AVE);
+    printf("VAL=%lf\n",VAL);
+
+    UVAL=(double)i/(double)(i-1)*VAL;//不偏分散を求める。
+
+    printf("UVAL=%lf",UVAL);
 
     return 0;
 
 
 }
 
+double ave_online(double val,double ave,int N){
+     double n= (double)N;//整数の割り算ができないので、double型にキャスト変換しておく。
+    return((n-1)/n*ave+val/n);
+}
+
+double var_online(double val, double ave, double square_ave,int N){
+    double n= (double)N;//整数の割り算ができないので、double型にキャスト変換しておく。
+    return((n-1)/n*square_ave+(val*val)/n-pow(((n-1)/n*ave+val/n),2));
+}
